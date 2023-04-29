d9216bbc14cac631a62c17bbc18909f36253c2fb
shinigami <dynamitecrate@gmail.com>
2023-04-30 01:36:25 +0530

    Engagement Rate for Youtube updated.

--- youtube/engagement_rate.py
+++ youtube/engagement_rate.py
@@ -1,46 +1,50 @@
 import random
 import csv
+import datetime
 
-# Define the average engagement rate
-avg_engagement_rate = 0.05
+# Set up the starting values
+subscriber_count = 1000
+total_views = 2000
+likes_count = 500
+dislikes_count = 20
 
-# Define the number of videos
-num_videos = 100
+# Set up the headers for the CSV file
+headers = ["Date", "Subscriber Count", "Total Views", "Likes Count", "Dislikes Count", "Engagement Rate"]
 
-# Define the start date
-start_date = '2022-01-01'
+# Set up the start and end dates
+start_date = "2022-01-01"
+end_date = "2023-12-31"
 
-# Define the header for the CSV file
-header = ['Video ID', 'Publish Date', 'Views', 'Likes', 'Dislikes', 'Comments', 'Engagement Rate']
+# Create a list of dates between the start and end dates
+date_list = []
+current_date = start_date
+while current_date <= end_date:
+    date_list.append(current_date)
+    current_date = (datetime.datetime.strptime(current_date, "%Y-%m-%d") + datetime.timedelta(days=1)).strftime("%Y-%m-%d")
 
-# Create an empty list to store the data
-data = []
-
-# Generate sample data for each video
-for i in range(num_videos):
-    # Generate a random number of views between 100 and 100,000
-    views = random.randint(100, 100000)
-
-    # Generate a random number of likes between 0 and the number of views
-    likes = random.randint(0, views)
-
-    # Generate a random number of dislikes between 0 and the number of views minus the number of likes
-    dislikes = random.randint(0, views - likes)
-
-    # Generate a random number of comments between 0 and 10% of the number of views
-    comments = random.randint(0, int(0.1 * views))
-
-    # Calculate the engagement rate
-    engagement_rate = (likes + dislikes + comments) / views
-
-    # Apply the average engagement rate to the actual engagement rate to introduce some variability
-    engagement_rate *= random.uniform(0.5, 1.5) * avg_engagement_rate
-
-    # Add the data to the list
-    data.append([i + 1, start_date, views, likes, dislikes, comments, engagement_rate])
-
-# Write the data to a CSV file
-with open('youtube_engagement_rate.csv', mode='w', newline='') as file:
+# Set up the CSV file
+with open("youtube_data.csv", "w", newline="") as file:
     writer = csv.writer(file)
-    writer.writerow(header)
-    writer.writerows(data)
+    writer.writerow(headers)
+
+    # Generate the data for each day
+    for date in date_list:
+        # Generate random engagement rate between 1% and 10%
+        engagement_rate = random.uniform(0.01, 0.1)
+
+        # Calculate the number of likes and dislikes based on the engagement rate
+        likes = int(total_views * engagement_rate * random.uniform(0.8, 1.2))
+        dislikes = int(likes * random.uniform(0.01, 0.1))
+
+        # Calculate the new total views based on the engagement rate
+        total_views = int(total_views * (1 + engagement_rate * random.uniform(0.8, 1.2)))
+
+        # Calculate the new subscriber count based on the engagement rate
+        subscriber_count = int(subscriber_count * (1 + engagement_rate * random.uniform(0.8, 1.2)))
+
+        # Update the likes and dislikes count
+        likes_count += likes
+        dislikes_count += dislikes
+
+        # Write the data to the CSV file
+        writer.writerow([date, subscriber_count, total_views, likes_count, dislikes_count, engagement_rate])
